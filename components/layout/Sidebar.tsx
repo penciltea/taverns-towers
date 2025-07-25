1a3e9d94ed58b900864fa759463648f504f0c1e0
Brooke Yombalakian <penciltea@aol.com>
2025-07-24 22:03:12 -0400

    Adjusting sidebar nav to be accordions with links or onclick events, fixing conflicts with bos, mongodb, and mongoose

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -2,30 +2,88 @@
 
 import { useRouter } from "next/navigation";
 import { useUIStore } from "@/store/uiStore";
-import { Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Typography, useMediaQuery, useTheme } from "@mui/material";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-
-
-const drawerWidth = 240;
-const navItems = [
-    { label: "Settlements", path: "/settlements/all", disabled: false },
-    { label: "Sites", path: "/sites", disabled: false },
-    { label: "NPCs", path: "/npcs", disabled: true },
-    { label: "Guilds", path: "/guilds", disabled: true }
-];
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export const Sidebar = () => {
     const router = useRouter();
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
+    const { setOpenDialog } = useUIStore();
     const isDrawerOpen = useUIStore((state) => state.isDrawerOpen);
     const closeDrawer = useUIStore((state) => state.closeDrawer);
 
-    const handleNavigation = (path: string, disabled: boolean) => {
-        if (disabled) return;
-        router.push(path);
-        if (isMobile) closeDrawer();
+    const drawerWidth = 240;
+    const navItems = [
+        {
+            label: "Settlements",
+            children: [
+            {
+                label: "View all settlements",
+                path: "/settlements/all",
+                enabled: true
+            },
+            {
+                label: "Create settlement",
+                enabled: true,
+                path: "/settlements/new"
+            }
+            ]
+        },
+        {
+            label: "Sites",
+            children: [
+            {
+                label: "View all sites",
+                path: "/sites",
+                enabled: true
+            },
+            {
+                label: "Create site",
+                enabled: true,
+                onClick: () =>
+                setOpenDialog("siteTypeDialog", {
+                    dialogMode: "global"
+                })
+            }
+            ]
+        },
+        {
+            label: "NPCs",
+            children: [
+                {
+                    label: "View all NPCs",
+                    path: "/npcs",
+                    enabled: false
+                },
+            ]
+        },
+        {
+            label: "Guilds",
+            children: [
+                {
+                    label: "View all Guilds",
+                    path: "/guilds",
+                    enabled: false
+                },
+            ]
+        }
+    ];
+
+    const handleNavigation = (
+        path?: string,
+        enabled?: boolean,
+        onClick?: () => void
+    ) => {
+        if (!enabled) return;
+        if (onClick) {
+            onClick();
+        } else if (path) {
+            router.push(path);
+            if (isMobile) closeDrawer();
+        }
     };
 
     const drawerContent = (
@@ -38,16 +96,26 @@ export const Sidebar = () => {
             </IconButton>
             )}
         </Box>
-        <List>
-            {navItems.map(({ label, path, disabled }) => (
-            <ListItem key={label} disablePadding>
-                <ListItemButton
-                onClick={() => handleNavigation(path, disabled)}
-                disabled={disabled}
-                >
-                <ListItemText primary={label} />
-                </ListItemButton>
-            </ListItem>
+        <List disablePadding>
+            {navItems.map((item) => (
+            <Accordion key={item.label} disableGutters elevation={0} square>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography variant="subtitle1">{item.label}</Typography>
+                </AccordionSummary>
+                <AccordionDetails sx={{ py: 0 }}>
+                {item.children.map((child) => (
+                    <ListItemButton
+                        key={child.label}
+                        onClick={() =>
+                            handleNavigation(child.path, child.enabled, child.onClick)
+                        }
+                        disabled={!child.enabled}
+                    >
+                        <ListItemText primary={child.label} />
+                    </ListItemButton>
+                ))}
+                </AccordionDetails>
+            </Accordion>
             ))}
         </List>
         </Box>
